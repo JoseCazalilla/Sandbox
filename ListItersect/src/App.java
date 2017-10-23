75fa41c6db80edb877cfbc9907f0f24f97c325df
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-23 20:08:55 +0200

    First design...

--- ListItersect/src/App.java
+++ ListItersect/src/App.java
@@ -5,23 +5,37 @@ import java.awt.event.ActionListener;
 
 public class App {
     private JPanel panelMain;
+    private JButton runButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JTextArea matchedTextArea;
+    private JTextArea executionTextArea;
     private JButton buttonRun;
 
     public App() {
-        buttonRun.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Fooo");
 
+        comboBox1.addItem("Set A");
+        comboBox1.addItem("Set B");
+
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                matchedTextArea.setText(comboBox1.getSelectedItem().toString());
             }
         });
     }
 
     public static void main(String[] args) {
+
+
+
         JFrame frame = new JFrame("App");
         frame.setContentPane(new App().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,150);
+        frame.setSize(700,400);
         frame.setVisible(true);
     }
 }
