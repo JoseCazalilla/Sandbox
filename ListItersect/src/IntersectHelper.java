cc45b2d77e5953165aec860d874f0b83729a6f69
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-23 17:01:07 +0200

    Adding first skeleton for IntersectHelper Class

---
+++ ListItersect/src/IntersectHelper.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class IntersectHelper {
+    private ArrayList a;
+    private HashSet h;
+
+    // Public constructor
+    public IntersectHelper() {
+        a = new ArrayList();
+        h = new HashSet();
+    }
+
+    // This method will fill the array with the info provided
+    public void fillArray(int numElems, int maxNum) {
+
+    }
+
+    // This method will fill the HashSet with the info provided
+    public void fillHashset(int numElems, int maxNum) {
+
+    }
+
+    // This method will return an ArrayList with the result
+    public ArrayList calcIntersection() {
+
+        ArrayList foo = new ArrayList();
+        return foo;
+    }
+
+    // This method will clear both the array and the hashSet (just in case we need to use it)
+    public void reset() {
+
+    }
+}
