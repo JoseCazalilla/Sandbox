b37fa6f646d47be681cca70dc9217eea24a669bc
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-25 08:53:26 +0200

    Adding remaining functions. Now, it is complete.

--- KotlinListINtersect/src/IntersectHelper.kt
+++ KotlinListINtersect/src/IntersectHelper.kt
@@ -5,6 +5,7 @@ class IntersectHelper {
     private val a:ArrayList<Int>
     private val h:HashSet<Int>
     private val res:HashSet<Int>
+
     // Public constructor
     init{
         a = ArrayList()
@@ -18,8 +19,50 @@ class IntersectHelper {
         }
     }
 
-    fun test(){
-        for (item in a) print("$item ")
+    fun fillHashset(numElems: Int, maxNum: Int) {
+        for (i in 0 until numElems) {
+            h.add((Math.random() * maxNum).toInt())
+        }
+    }
+
+    fun calcIntersection(): Pair<Long, HashSet<Int>> {
+
+        // Getting the timestamp here
+        val startTime = System.currentTimeMillis()
+
+        for (i in a) {
+            if (h.contains(i)) {
+                res.add(i)
+            }
+        }
+        // Getting the timestamp here
+        val stopTime = System.currentTimeMillis()
+
+        return Pair(stopTime - startTime, res)
+    }
+
+    fun calcIntersectionForLoop(): Pair<Long, HashSet<Int>> {
+
+        // Getting the timestamp here
+        val startTime = System.currentTimeMillis()
+
+        for (i in a) {
+            for (j in h) {
+                if (i.equals(j)) {
+                    res.add(i)
+                }
+            }
+        }
+        // Getting the timestamp here
+        val stopTime = System.currentTimeMillis()
+
+        return Pair(stopTime - startTime, res)
+    }
+
+    fun reset() {
+        a.clear()
+        h.clear()
+        res.clear()
     }
 
 }
--- KotlinListINtersect/src/Main.kt
+++ KotlinListINtersect/src/Main.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
-    val myIntersecHelper = IntersectHelper()
-    myIntersecHelper.fillArray(20,20)
-    myIntersecHelper.test()
+    val myIntersectHelper = IntersectHelper()
+    myIntersectHelper.fillArray(2000,2000)
+    myIntersectHelper.fillHashset(2000,2000)
+
+    val (execTime, res) = myIntersectHelper.calcIntersection()
+//    val (execTime, res) = myIntersectHelper.calcIntersectionForLoop()
+
+    println("Execution time (ms): $execTime \t ElemsMatched: ${res.size}")
 }
