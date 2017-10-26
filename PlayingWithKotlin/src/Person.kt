d29e88145cdfa3ca7a3e66399a6f981d90dd3aa6
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-26 20:31:22 +0200

    Playing with inheritance

--- PlayingWithKotlin/src/Person.kt
+++ PlayingWithKotlin/src/Person.kt
@@ -18,6 +18,22 @@ class child(name: String): Person(name, 12) {
     override fun makeOlder(){
         println("Nevermind!")
     }
+}
 
+open class A {
+    open fun f() { print("A") }
+    fun a() { print("a") }
+}
 
+interface B {
+    fun f() { print("B") } // interface members are 'open' by default
+    fun b() { print("b") }
+}
+
+class C() : A(), B {
+    // The compiler requires f() to be overridden:
+    override fun f() {
+        super<A>.f() // call to A.f()
+        super<B>.f() // call to B.f()
+    }
 }
