3f98e108e3e126f717f7fcd159308a207705b7cc
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-29 10:46:24 +0100

    Overloading Comparable

--- PlayingWithKotlin/src/Person.kt
+++ PlayingWithKotlin/src/Person.kt
@@ -52,3 +52,13 @@ fun Int.r(): RationalNumber = RationalNumber(this, 1)
 fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+}
+
+fun compare(date1: MyDate, date2: MyDate) = date1 < date2
