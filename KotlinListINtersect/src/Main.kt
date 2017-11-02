f910d6fd8162b7c7d18d364a35cbf74ad24ae158
Jose Cazalilla <jocamo5upv@gmail.com>
2017-11-02 10:09:49 +0100

    Moving the array creation into an apply HOF

--- KotlinListINtersect/src/Main.kt
+++ KotlinListINtersect/src/Main.kt
@@ -1,10 +1,12 @@
 fun main(args: Array<String>) {
-    val myIntersectHelper = IntersectHelper()
-    myIntersectHelper.fillArray(2000,2000)
-    myIntersectHelper.fillHashset(2000,2000)
 
-    val (execTime, res) = myIntersectHelper.calcIntersection()
-//    val (execTime, res) = myIntersectHelper.calcIntersectionForLoop()
+    val myIntersectHelper = IntersectHelper().apply {
+        fillArray(10000,5000)
+        fillHashset(10000,5000)
+    }
+
+       val (execTime, res) = myIntersectHelper.calcIntersection()
+    //    val (execTime, res) = myIntersectHelper.calcIntersectionForLoop()
 
     println("Execution time (ms): $execTime \t ElemsMatched: ${res.size}")
  }
